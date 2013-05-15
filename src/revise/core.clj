844a265a00a11e32dee27bad3d9c8ab1a9e4393d
Chris Allen <cma@bitemyapp.com>
2013-05-14 17:55:00 -0700

    progress. ish.

--- src/revise/core.clj
+++ src/revise/core.clj
@@ -8,14 +8,64 @@
   (:gen-class :main true))
 
 (import Rethinkdb$VersionDummy)
+(import Rethinkdb$Response)
+(import Rethinkdb$Query)
+(import Rethinkdb$Datum)
+(import Rethinkdb$Term)
+(import Rethinkdb$Backtrace)
+(import Rethinkdb$Frame)
 
 (def version-frame (compile-frame {:a :int32-le}))
 (defn gen-ver [val]
   (.array (contiguous (encode version-frame {:a val}))))
 
 (def VersionDummy (protodef Rethinkdb$VersionDummy))
+(def Response (protodef Rethinkdb$Response))
+(def Datum (protodef Rethinkdb$Datum))
+(def Query (protodef Rethinkdb$Query))
+(def Frame (protodef Rethinkdb$Frame))
+(def Backtrace (protodef Rethinkdb$Backtrace))
+(def Term (protodef Rethinkdb$Term))
 
 (def db-host {:name "localhost" :port 28015})
+
+(defn pb [& args]
+  (protobuf args))
+(defn pbt [& args]
+  (pb Term args))
+(defn pbd [& args]
+  (pb Datum args))
+
+(defn list-databases []
+  (protobuf-dump
+   (pbt :type :DB_LIST)))
+
+(defn insert-example []
+  (protobuf-dump
+   (protobuf Term
+             :type :INSERT
+             :args [(protobuf Term
+                              :type :TABLE
+                              :args [(protobuf Term
+                                               :type :DATUM
+                                               :r_datum (protobuf Datum
+                                                                  :type :R_STR
+                                                                  :r_str "test"))]
+                              :optargs [["use_outdated" (protobuf Term
+                                                                  :type :DATUM
+                                                                  :r_datum (protobuf Datum :type :R_BOOL :r_bool true))]])
+                    (protobuf Term
+                              :type :MAKE_ARRAY
+                              :args [(protobuf Term
+                                               :type :DATUM
+                                               :r_datum (protobuf Datum
+                                                                  :type :R_OBJECT
+                                                                  :r_object [["id", 0]]))
+                                     (protobuf Term
+                                               :type :DATUM
+                                               :r_datum (protobuf Datum
+                                                                  :type :R_OBJECT
+                                                                  :r_object [["id", 1]]))])])))
 
 (def magic-number 1063369270)
 (def version (protobuf-dump (protobuf VersionDummy :Version magic-number)))
@@ -39,15 +89,25 @@
     (.write data)
     (.flush)))
 
-(defn conn-handler [conn]
-  (while (nil? (:exit @conn))
-    (let [data (.readLine (:in @conn))]
-      (when data
-        (println data)))))
-
 (def db (connect db-host))
+
+(defn conn-handler []
+  (println "Conn handler started")
+  (while (nil? (:exit @db))
+    (let [data (.readLine (:in @db))]
+      (when data
+        (let [pb (protobuf-load Response data)
+              bpb (bean pb)]
+          (println "data read in")
+          (println data)
+          (println pb)
+          (println bpb))))))
 
 (defn -main [& args]
   ;; (write rdb magic-number)
+  (println "pre-handler")
+  (.start (Thread. conn-handler))
+  (println "post-handler")
   (write db ver-frame)
-  (conn-handler db))
+  ;; (write db (insert-example))
+  (write db (list-databases)))
