f5bf3a467bf3029c8b0457dbf6bad7a9a54a8aaa
cesarbp <cesar.bolanosportilla@gmail.com>
2013-10-30 23:40:46 -0600

    Fix small query mistakes, duct tape undefined errors

--- src/bitemyapp/revise/core.clj
+++ src/bitemyapp/revise/core.clj
@@ -1,13 +1,13 @@
 (ns bitemyapp.revise.core
   "Testing stuff"
   (:refer-clojure :exclude [send compile])
-  (:require [bitemyapp.revise.connection :refer [connect close send]]
-            [bitemyapp.revise.protoengine :refer [compile]]
+  (:require [bitemyapp.revise.connection :refer [connect close send-term]]
+            [bitemyapp.revise.protoengine :refer [compile-term]]
             [bitemyapp.revise.query :as r]))
 
 (defn run
   [q]
-  (send (compile q)))
+  (send-term (compile-term q)))
 
 (defn -main
   []
