fb9638b2dfcf2ab53d8bd34646db28360dbd05f0
cesarbp <cesar.bolanosportilla@gmail.com>
2014-05-05 12:17:27 -0500

    Removed dependency on lein-protobuf. Moved protobufs to their own jar. Forked clojure-protobuf to remove implicit dep on lein-protobuf

--- project.clj
+++ project.clj
@@ -1,16 +1,15 @@
-(defproject revise "0.0.6"
+(defproject revise "0.0.7-SNAPSHOT"
   :description "RethinkDB client for Clojure"
   :url "github.com/bitemyapp/revise/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main bitemyapp.revise.core
-  :plugins [[lein-protobuf "0.3.1" :exclusions [leinjacker]]
-            [com.jakemccrary/lein-test-refresh "0.1.2"]
+  :plugins [[com.jakemccrary/lein-test-refresh "0.1.2"]
             [lein-difftest "2.0.0"]]
   :test-selectors {:default (fn [_] true) ;; (complement :integration)
                    :race-condition :race-condition
                    :all (fn [_] true)}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [robert/bruce "0.7.1"]
-                 [org.flatland/protobuf "0.7.2"]]
-  :repl-options {:port 7779})
+                 [revise/protobuf "0.8.3"]
+                 [revise/rethinkdb "1.0.0"]])
